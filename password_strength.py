f036a39a9fe50afb7cd9d10f46eb0fe2525d1d0a
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-05-02 20:45:47 +0300

    Added getting of score of password strength with using zxcvbn library

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,20 @@
 import argparse
 
+from zxcvbn import zxcvbn
+
 
 def get_password_strength(password):
-    pass
+    password_analysis_result = zxcvbn(password)
+
+    score_password_strength = int(password_analysis_result['guesses_log10'])
+
+    if score_password_strength < 1:
+        score_password_strength = 1
+
+    if score_password_strength > 10:
+        score_password_strength = 10
+
+    return score_password_strength
 
 
 def parse_command_line_arguments():
