d85d9ba029e89306e8ebc6d12c37904a7b49bad1
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-05-02 19:45:42 +0300

    Added parsing of password from command line

--- password_strength.py
+++ password_strength.py
@@ -1,9 +1,28 @@
+import argparse
+
+
 def get_password_strength(password):
     pass
 
 
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'password',
+        help='a password for analysis',
+        type=str,
+    )
+
+    command_line_arguments = parser.parse_args()
+
+    return command_line_arguments
+
+
 def main():
-    pass
+    command_line_arguments = parse_command_line_arguments()
+
+    password = command_line_arguments.password
 
 
 if __name__ == '__main__':
